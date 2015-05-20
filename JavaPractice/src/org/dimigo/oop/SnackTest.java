eed318694ac1d5578866e6771c148025bb8f67e0
JS <JS@User>
2015-05-20 14:19:27 +0900

    10번쨰

--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -8,7 +8,7 @@ public class SnackTest {
 		Snack[] snackArr = new Snack[] {
 				new Snack("새우깡", "농심", 1100, 2),
 				new Snack("콘칩", "크라운", 1200, 1),
-				new Snack("허니버터칩", "해태", 500, 4)
+				new Snack("허니버터칩", "해태", 1500, 4)
 		};
 		
 		int sum = 0;  
