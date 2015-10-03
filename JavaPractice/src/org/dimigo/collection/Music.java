0dd1c54579836fee01103dd9a5539bb25739cf43
JS <JS@DESKTOP-UJVOU7B>
2015-10-03 17:13:35 +0900

    멜론

--- JavaPractice/src/org/dimigo/collection/Music.java
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -1,30 +1,38 @@
 package org.dimigo.collection;
 
 public class Music {
-	
-	private String title;
-	private String singer;
 
-	public Music(String title, String singer) {
-		this.title = title;
-		this.singer = singer;
-	}
+	private String title; 
+ 	private String singer; 
+ 	 
+ 	public Music(String title, String siger) { 
+ 		this.title = title; 
+		this.singer = siger; 
+	} 
 
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
+	 
+	public String getTitle() { 
+		return title; 
+	} 
 
-	public String getSinger() {
-		return singer;
-	}
-	public void setSinger(String singer) {
-		this.singer = singer;
-	}
+	 
+	public String getSinger() { 
+		return singer; 
+	} 
 
-	public String toString() {
-		return "Music [title=" + title + ", singer=" + singer + "]";
-	}
+	 
+	public void setTitle(String title) { 
+		this.title = title; 
+	} 
+
+	 
+	public void setSinger(String singer) { 
+		this.singer = singer; 
+	} 
+		 
+	@Override 
+	public String toString() { 
+		return "Music [title=" + title + ", singer=" + singer + "]";  
+	} 
+
 }
