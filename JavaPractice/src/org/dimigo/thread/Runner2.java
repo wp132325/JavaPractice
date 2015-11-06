4f8c74f6b885a9a994d71912da1c7259bb78dbbb
JS <JS@DESKTOP-UJVOU7B>
2015-11-06 14:28:26 +0900

    실습과제 10

---
+++ JavaPractice/src/org/dimigo/thread/Runner2.java
@@ -0,0 +1,33 @@
+package org.dimigo.thread;
+
+import java.util.Random;
+
+public class Runner2 implements Runnable{
+		private String name;
+		
+		public Runner2() {
+			
+		}
+		
+		public Runner2(String name) {
+			this.name = name;
+		}
+		
+		public void run() {
+			for(int i = 100;i >= 0;i-=10) {
+				int time = new Random().nextInt(1000);
+				
+				System.out.println(name + " " + i + "미터");
+				try {
+					Thread.sleep(time);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}
+			
+		}
+		
+	System.out.println(name + " 도착");
+	}
+		
+}
